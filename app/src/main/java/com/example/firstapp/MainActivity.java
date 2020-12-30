d02d326fe636da55a4126af33fb314d574f24183
mualiabbas <abbasm31@mcmaster.ca>
2020-12-30 06:37:29 -0500

    switch activities

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.firstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -14,11 +16,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText email = findViewById(R.id.email);
+        EditText userName = findViewById(R.id.userName);
+
         configureNextButton();
     }
 
     private void configureNextButton() {
-        Button  nextButton = (Button) findViewById(R.id.searchbut);
+        Button  searchBut = (Button) findViewById(R.id.searchbut);
+        searchBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Home.class));
+            }
+        });
     }
-
 }
